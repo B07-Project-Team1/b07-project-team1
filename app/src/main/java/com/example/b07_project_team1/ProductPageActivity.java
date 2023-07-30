d6608ac3ed697d94eaf355b8c942b71beca2bdb1
Alan <zalan.zhang@mail.utoronto.ca>
2023-07-30 02:02:00 -0400

    add counter functionality

--- app/src/main/java/com/example/b07_project_team1/ProductPageActivity.java
+++ app/src/main/java/com/example/b07_project_team1/ProductPageActivity.java
@@ -5,6 +5,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -18,18 +20,26 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.ktx.Firebase;
 
-public class ProductPageActivity extends AppCompatActivity {
+public class ProductPageActivity extends AppCompatActivity implements View.OnClickListener {
 
     ImageView storeLogo;
     ImageView productImage;
     TextView productName;
     TextView productInfo;
     TextView productPrice;
+    TextView productQuantity;
     String productImageUrl;
     Product product;
     String productId;
     String vendorId;
     Vendor vendor;
+    int count = 1;
+    Button backButton;
+    Button countButtonM;
+    Button countButtonP;
+    Button cartButton;
+    Button buyButton;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -58,7 +68,43 @@ public class ProductPageActivity extends AppCompatActivity {
         productPrice = (TextView) findViewById(R.id.product_page_product_price);
         productPrice.setText("$" + Double.toString(product.getPrice()));
 
+        productQuantity = (TextView) findViewById(R.id.product_page_quantity);
+        productQuantity.setText("1");
+
         loadVendorInfo();
+
+        backButton = (Button) findViewById(R.id.product_page_back_button);
+        backButton.setOnClickListener(this);
+        countButtonM = (Button) findViewById(R.id.product_page_minus_button);
+        countButtonM.setOnClickListener(this);
+        countButtonP = (Button) findViewById(R.id.product_page_plus_button);
+        countButtonP.setOnClickListener(this);
+        cartButton = (Button) findViewById(R.id.product_page_cart_button);
+        cartButton.setOnClickListener(this);
+        buyButton = (Button) findViewById((R.id.product_page_buy_button));
+        buyButton.setOnClickListener(this);
+        
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v.getId() == R.id.product_page_back_button) {
+            this.finish();
+        } else if (v.getId() == R.id.product_page_minus_button) {
+            if (count - 1 >= 1) {
+                count--;
+                productQuantity.setText(Integer.toString(count));
+            }
+        } else if (v.getId() == R.id.product_page_plus_button) {
+            if (count + 1 <= 99) {
+                count++;
+                productQuantity.setText(Integer.toString(count));
+            }
+        } else if (v.getId() == R.id.product_page_cart_button) {
+            //TODO
+        } else if (v.getId() == R.id.product_page_buy_button) {
+
+        }
     }
 
     void loadVendorInfo() {
