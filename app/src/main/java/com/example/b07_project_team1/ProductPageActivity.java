e271eab773eb3ffb9dad4b435c9aa0bde5dd884e
jang-35 <73324700+jang-35@users.noreply.github.com>
2023-07-30 20:15:02 -0400

    Implement product page UI and order flow for customer (#15)
    - Implement product page UI for customer and vendor view
    - Implement product information display
    - Implement single order functionality for customer
    - Implement storage of order data in database
    
    
    Co-authored-by: wobertt <robertchung1462@gmail.com>

--- app/src/main/java/com/example/b07_project_team1/ProductPageActivity.java
+++ app/src/main/java/com/example/b07_project_team1/ProductPageActivity.java
@@ -4,27 +4,47 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
+import com.example.b07_project_team1.model.Order;
 import com.example.b07_project_team1.model.Product;
 import com.example.b07_project_team1.model.Vendor;
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.ktx.Firebase;
+import com.google.firebase.database.ValueEventListener;
 
-public class ProductPageActivity extends AppCompatActivity {
+import java.util.HashMap;
+
+public class ProductPageActivity extends AppCompatActivity implements View.OnClickListener {
 
     ImageView storeLogo;
+    ImageView productImage;
+    TextView productName;
+    TextView productInfo;
+    TextView productPrice;
+    TextView productQuantity;
     String productImageUrl;
     Product product;
     String productId;
     String vendorId;
     Vendor vendor;
+    int count = 1;
+    Button backButton;
+    Button countButtonM;
+    Button countButtonP;
+    Button cartButton;
+    Button buyButton;
+    DatabaseReference ref;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -35,12 +55,103 @@ public class ProductPageActivity extends AppCompatActivity {
         Bundle bundle = getIntent().getExtras();
         assert bundle != null;
 
-        productImageUrl = bundle.getString("IMAGE_URL");
+        productImageUrl = bundle.getString("PRODUCT_IMAGE");
         product = bundle.getSerializable("PRODUCT_INFO", Product.class);
         productId = bundle.getString("PRODUCT_ID");
         vendorId = product.getVendorId();
 
+        productImage = findViewById(R.id.product_page_image);
+        Glide.with(ProductPageActivity.this).load(productImageUrl).into(productImage);
+
+        productName = findViewById(R.id.product_page_product_name);
+        productName.setText(product.getProductName());
+
+        productInfo = findViewById(R.id.product_page_product_info);
+        productInfo.setText(product.getDescription());
+
+        productPrice = findViewById(R.id.product_page_product_price);
+        productPrice.setText("$" + String.format("%.2f", (product.getPrice())));
+
+        productQuantity = findViewById(R.id.product_page_quantity);
+        productQuantity.setText("1");
+
+        ref = FirebaseDatabase.getInstance().getReference();
         loadVendorInfo();
+
+        backButton = findViewById(R.id.product_page_back_button);
+        backButton.setOnClickListener(this);
+        countButtonM = findViewById(R.id.product_page_minus_button);
+        countButtonM.setOnClickListener(this);
+        countButtonP = findViewById(R.id.product_page_plus_button);
+        countButtonP.setOnClickListener(this);
+        cartButton = findViewById(R.id.product_page_cart_button);
+        cartButton.setOnClickListener(this);
+        buyButton = findViewById((R.id.product_page_buy_button));
+        buyButton.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v.getId() == R.id.product_page_back_button) {
+            this.finish();
+        } else if (v.getId() == R.id.product_page_minus_button) {
+            if (count - 1 >= 1) {
+                count--;
+                productQuantity.setText(Integer.toString(count));
+            }
+        } else if (v.getId() == R.id.product_page_plus_button) {
+            if (count + 1 <= 99) {
+                count++;
+                productQuantity.setText(Integer.toString(count));
+            }
+        } else if (v.getId() == R.id.product_page_cart_button) {
+            //TODO issue-8
+        } else if (v.getId() == R.id.product_page_buy_button) {
+            addOrder(count);
+        }
+    }
+
+    void addOrder(int quantity) {
+
+        FirebaseAuth mAuth = FirebaseAuth.getInstance();
+        FirebaseUser user = mAuth.getCurrentUser();
+        assert user != null;
+        String uid = user.getUid();
+        ValueEventListener eventListener = new ValueEventListener() {//place order iff user is customer
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                if (dataSnapshot.exists()) {//check user id exists in customer records
+                    //create order and write to db
+                    HashMap<String, Integer> items = new HashMap<>();
+                    items.put(product.getProductName(), quantity);
+                    Order order = new Order(uid, vendorId, items, false);
+                    DatabaseReference newOrderRef = ref.child("orders").push();
+                    newOrderRef.setValue(order);
+
+                    String orderId = newOrderRef.getKey();
+                    assert orderId != null;
+
+                    //update vendor+customer accordingly
+                    ref.child("vendors").child(vendorId).child("orders").child(orderId).setValue(true);
+                    ref.child("customers").child(uid).child("pending_orders").child(orderId).setValue(true);
+
+                    showToast("Order success!");
+                } else {
+                    showToast("Must be customer to purchase!");
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+            }
+        };
+        ref.child("customers").child(uid).addListenerForSingleValueEvent(eventListener);
+    }
+
+    void showToast(CharSequence text) {
+        Toast toast = Toast.makeText(ProductPageActivity.this, text, Toast.LENGTH_SHORT);
+        toast.show();
     }
 
     void loadVendorInfo() {
