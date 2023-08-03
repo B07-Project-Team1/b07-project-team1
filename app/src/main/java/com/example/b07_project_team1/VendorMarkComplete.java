749884fb4227ce3e538a5ae72e94abdfe5117393
wobertt <robertchung1462@gmail.com>
2023-08-03 13:34:59 -0400

    Add customer and vendor table updates to backend for order completion

--- app/src/main/java/com/example/b07_project_team1/VendorMarkComplete.java
+++ app/src/main/java/com/example/b07_project_team1/VendorMarkComplete.java
@@ -11,26 +11,42 @@ import com.google.android.gms.tasks.Task;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class VendorMarkComplete {
     public void onClickMarkComplete() {
 
     }
 
-    public void markOrderAsComplete(String orderId) {
+    public void markOrderAsComplete(String orderId, String vendorId, String customerId) {
         DatabaseReference ref = FirebaseDatabase.getInstance().getReference();
-        ref.child("orders").child(orderId).setValue(true).addOnCompleteListener(task -> {
+        /*
+        Database writes:
+         orders/orderId/completed, true
+         vendors/vendorId/orders/orderId, null
+         customers/customerId/pending_orders/orderId, null
+         customers/customerId/completed_orders/orderId, true
+        */
+        Map<String, Object> updates = new HashMap<>();
+        updates.put(String.format("orders/%s/completed", orderId), true);
+        updates.put(String.format("vendors/%s/orders/%s", vendorId, orderId), null);
+        updates.put(String.format("customers/%s/pending_orders/%s", customerId, orderId), null);
+        updates.put(String.format("customers/%s/completed_orders/%s", customerId, orderId), true);
+        ref.updateChildren(updates).addOnCompleteListener(task -> {
             if (task.isSuccessful()) {
-                showSuccessToast();
+                showToast("Order completed!");
             } else {
                 Log.e("FIREBASE", "Couldn't mark order as complete");
+                showToast("Error: Couldn't complete order");
             }
         });
     }
 
-    public void showSuccessToast() {
+    public void showToast(CharSequence text) {
         // TODO: Change the context to the correct class
         Context context = null;
-        Toast toast = Toast.makeText(context, "Order completed!", Toast.LENGTH_SHORT);
+        Toast toast = Toast.makeText(context, text, Toast.LENGTH_SHORT);
         toast.show();
     }
 
