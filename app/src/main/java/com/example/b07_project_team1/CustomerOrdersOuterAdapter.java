49c2c888d02c6b6655e435b9fe903a0fb3a5ab9a
Alan <zalan.zhang@mail.utoronto.ca>
2023-08-09 08:22:42 -0400

    remove unnecessary imports

--- app/src/main/java/com/example/b07_project_team1/CustomerOrdersOuterAdapter.java
+++ app/src/main/java/com/example/b07_project_team1/CustomerOrdersOuterAdapter.java
@@ -1,7 +1,7 @@
 package com.example.b07_project_team1;
 
 import android.content.Context;
-import android.graphics.Color;
+
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
